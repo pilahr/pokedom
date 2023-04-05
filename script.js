b86a3899f685cc0bb3d37ecbe0e53cb9c3df2fa6
Piyaporn <piyapornlahr@gmail.com>
2023-04-05 16:48:29 +0100

    final touch

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ import pokemonArray from "./data/pokemon.js";
 const cardContainer = document.querySelector(".card-container");
 
 const renderCard = (pokemonObject) => {
-  const { id, name, types, sprite } = pokemonObject;
+  const { name, types, sprite } = pokemonObject;
+
+  return `<div style="width:100%" class="card">
+  <img class="card__image" src=${sprite} />
+  <div class="card__content">
+    <h2 class="card__heading">${name}</h2>
+    <p class="card__text">${types}</p>
+  </div>`;
 };
 
 pokemonArray.forEach((pokemon) => {
