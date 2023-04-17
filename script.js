39f9cc9079d17a581516401fd236f42c6bd45341
Piyaporn <piyapornlahr@gmail.com>
2023-04-17 09:03:01 +0100

    applied some more information on the pokemon type

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import pokemonArray from "./data/pokemon.js";
 const cardContainer = document.querySelector(".card-container");
 
 const renderCard = (pokemonObject) => {
-  const { name, types, sprite } = pokemonObject;
+  const { name, types, sprite, id } = pokemonObject;
 
   return `<div style="width:100%" class="card">
   <img class="card__image" src=${sprite} />
   <div class="card__content">
     <h2 class="card__heading">${name}</h2>
-    <p class="card__text">${types}</p>
+    <p class="card__text">${name} (${id}) is a ${types} type pokemon.</p>
   </div>`;
 };
 
